716b165953296a45fd27526df1cced638d5b9bdf
saira512dev <512saira@gmail.com>
2025-10-08 15:43:10 +0530

    Set up appwrite config

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.jsm.fooddelivery",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68e5f46a0020932a4591',
+    userCollectionId: 'user',
+}
