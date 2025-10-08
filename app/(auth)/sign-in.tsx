0ffcedbea14034bfdf73d9123f046edf854aacb0
saira512dev <512saira@gmail.com>
2025-10-08 15:42:23 +0530

    Add layout and pages for authentication

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,54 @@
-import { router } from "expo-router";
-import React from "react";
-import { Button, Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const [form, setform] = useState({ email: "", password: "" });
+
+  const submit = async () => {
+    if (!form.email || !form.password)
+      Alert.alert("Error", "Please enter valid email and password");
+
+    setisSubmitting(true);
+
+    try {
+      Alert.alert("Success", "User Signed In Successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setisSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button title="Sign In" onPress={() => router.push("/sign-up")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your email"
+        value={""}
+        onChangeText={(text) => {}}
+        label="email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={""}
+        onChangeText={(text) => {}}
+        label="password"
+        secureTextEntry={true}
+      />
+      <CustomButton title="Sign In" />
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Don't have an account?
+        </Text>
+        <Link href="/sign-up" className="base-bold text-primary">
+          Sign Up
+        </Link>
+      </View>
     </View>
   );
 };
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Redirect, Slot } from "expo-router";
 import React from "react";
 
 export default function _Layout() {
-  const isAuthenticated = false;
+  const isAuthenticated = true;
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
   return <Slot />;
 }
