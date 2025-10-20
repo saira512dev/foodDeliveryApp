633338c40a73d1f311e735f1a9a56f5aa84923c4
saira512dev <512saira@gmail.com>
2025-10-20 11:42:39 +0530

    Start with the search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect } from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
+  const { category, query } = useLocalSearchParams<{
+    query?: string;
+    category?: string;
+  }>();
+
+  const { data, refetch, loading } = useAppwrite({
+    fn: getMenu,
+    params: {
+      category,
+      query,
+      limit: 6,
+    },
+  });
+
+  const { data: categories } = useAppwrite({ fn: getCategories });
+
+  useEffect(() => {
+    refetch({ category, query, limit: 6 });
+  }, [category, query]);
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>Search</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
